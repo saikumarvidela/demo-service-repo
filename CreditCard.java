8cba2eab1c76b3f4230876fe1971f1a81d8ff325
saikumarvidela <62796104+saikumarvidela@users.noreply.github.com>
2024-01-02 00:44:27 +0530

    Update CreditCard.java

--- CreditCard.java
+++ CreditCard.java
@@ -1,6 +1,6 @@
 public class CreditCard{
 
-public static void main(){
-	System.out.println("Credit Card Application");
+public static void main(String[] args){
+	System.out.println("..............Credit Card Application.......");
 }
 }
