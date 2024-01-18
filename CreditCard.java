bd0fbd0c8793decbaf88528559b83d21e3f865aa
saikumarvidela <62796104+saikumarvidela@users.noreply.github.com>
2024-01-19 01:11:01 +0530

    Update CreditCard.java

--- CreditCard.java
+++ CreditCard.java
@@ -2,7 +2,7 @@ public class CreditCard{
 
 public static void main(String[] args){
 
-	System.out.println("Credit Card Application for HDFC");
+	System.out.println("Credit Card Application for HDFC and SBi");
 }
 
 }
